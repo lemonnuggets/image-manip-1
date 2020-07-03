ba32a533a9087b00c737fa26c8614f58272fe6c7
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2020-07-03 16:58:36 +0530

    added mouse interaction

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let colors = []
 let resolution = 75
 function preload () {
   // piece = loadImage('assets/download.jpeg')
-  piece = loadImage('assets/piece1.jpg')
+  piece = loadImage('assets/piece2.jpg')
 }
 function setup () {
   createCanvas(piece.width, piece.height)
@@ -43,7 +43,10 @@ function setup () {
   }
 }
 function draw () {
-  background(255)
+  background(0)
+  let x = mouseX
+  let y = mouseY
+  let diagonal = sqDistance(0, 0, width, height)
   // image(piece, 0, 0)
   let reColor = []
   for (
@@ -61,17 +64,18 @@ function draw () {
       noStroke()
       fill(c[0], c[1], c[2])
       // rect(cornerX, cornerY, cellSize, cellSize)
-      let centreX = cornerX + cellSize / 2
+      let centreX = cornerX + cellSize
       let centreY = cornerY + cellSize / 2
-      ellipse(
-        centreX,
-        centreY,
-        cellSize / 2, //random(1, 2),
-        cellSize / 2 //random(1, 2)
-      )
+      let distance = sqDistance(centreX, centreY, x, y)
+      let sf = map(distance, 0, diagonal, 1, 10, true)
+      let radius = cellSize / sf
+      ellipse(centreX, centreY, radius, radius, 2)
       pop()
       reColor.push(c)
     }
   }
   colors = reColor
+}
+function sqDistance (x1, y1, x2, y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
 }
